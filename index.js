9534033466c79dc8c2965e460a30bac2198866f5
Jhonatas Amorim <71660678+Jhonatas-Amorim@users.noreply.github.com>
2024-01-01 22:02:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -47,7 +47,7 @@
             })
         })
 
-//Abertura de servidor na porta 8081
+/*Abertura de servidor na porta 8081
     app.listen(8081,function(){
         console.log("Servidor Rodando!!")
-    });
+    });*/
